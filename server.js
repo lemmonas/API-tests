9f9ccb879482c61174feaa6fc352674965870974
lemmonas <lem20016@byui.edu>
2023-06-15 10:16:52 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to Andra's backend application");
+});// forward slash means the context root of the application, or the url without anything after it
+
 app.get("/lorenzo", (req, res)=> {res.send("Hello Lorenzo!");});
 
 app.get('/micah', (req,res) => {res.send("Hello Micah!");});
